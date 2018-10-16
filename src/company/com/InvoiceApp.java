73c190c1c4e94df712d98ae95bb6be1f0a9fb65e
GBTC406006UR <GBTC406006UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-10-16 17:06:23 -0400

    initial commit

--- src/company/com/InvoiceApp.java
+++ src/company/com/InvoiceApp.java
@@ -3,6 +3,7 @@ package company.com;
 
 
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class InvoiceApp {
@@ -10,17 +11,29 @@ public class InvoiceApp {
 
     public static void main(String[] args) {
 
+        Random rn= new Random();
+
+
         double totalPrice = 0;
+        double totalInvoice=0;
+        int totalQuantity=0;
         String stop = "";
 
-        String userInputItemName = "";
-
+       String userInputItemName = "";
+//
         Scanner sc = new Scanner(System.in);
+//
 
         ArrayList<Invoice> myInvoice = new ArrayList<Invoice>();
+        int  userInputQuantit=rn.nextInt(10);
+
+
+
 
 
         do {
+
+
             System.out.println("pleas enter the Item Name");
             userInputItemName = sc.next();
 
@@ -28,15 +41,31 @@ public class InvoiceApp {
             System.out.println("enter the price");
 
             double userInputPrice = sc.nextDouble();
-            System.out.println("enter the quantity");
-            int userInputQuantit = sc.nextInt();
+
+
+
+
+
+           // System.out.println("enter the quantity");
+
+            //int userInputQuantit = sc.nextInt();
 
             totalPrice = userInputQuantit * userInputPrice;
 
 
-            myInvoice.add(new Invoice(userInputItemName, userInputPrice, userInputQuantit, totalPrice));
+            totalInvoice=totalInvoice+totalPrice;
+            totalQuantity=totalQuantity+userInputQuantit;
 
-            Invoice invoice1 = new Invoice();
+               myInvoice.add(new Invoice(userInputItemName, userInputPrice,  userInputQuantit, totalPrice));
+
+
+
+
+
+
+
+
+            // Invoice invoice1 = new Invoice();
 
             System.out.println("Do you want to add more invoice ? yes/No");
             stop = sc.next();
@@ -52,5 +81,19 @@ public class InvoiceApp {
             System.out.println(invoceprnt.getItemName() + "\t\t" + invoceprnt.getPrice() + "\t\t" + invoceprnt.getQuantity() + "\t\t" + invoceprnt.getTotalPrice());
 
         }
+
+        System.out.println("total purchase amount "+"\t"+ totalInvoice);
+        System.out.println("total purchase quantit " + totalQuantity);
+
+
+        if (totalInvoice>20){
+            System.out.println("You spend to much");
+        }else {
+            System.out.println("Thank you for Shopping at my market");
         }
+
+
+
+
+    }
     }
